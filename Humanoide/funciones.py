b82e5b05f20522e58e9f12d1b290af213a459594
rulopelon <48516780+rulopelon@users.noreply.github.com>
2019-04-17 12:56:12 +0200

    pequeñas mejoras en la funcion mover_servo

--- Humanoide/funciones.py
+++ Humanoide/funciones.py
@@ -1,6 +1,10 @@
 def mover_servo(n_servo,pos):
     #posicion la reciviremos en un rango de 0 a 4096
-    driver.set_pwm(n_servo, 0, pos)
+    RANGO_MAXIMO = 4096
+    if pos > RANGO_MAXIMO:
+        print("no se puede mover ese rango, esta fuera del alcance")
+    elif pos< RANGO_MAXIMO:
+        driver.set_pwm(n_servo, 0, pos)
     return
 
 def acel_giro():
