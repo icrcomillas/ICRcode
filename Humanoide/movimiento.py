f1fc05e7b84e865ea2956288ae7d9b1aefa982c7
rulopelon <48516780+rulopelon@users.noreply.github.com>
2019-05-21 11:54:46 +0200

    .

--- Humanoide/movimiento.py
+++ Humanoide/movimiento.py
@@ -1,32 +1,32 @@
-    import time
-    import mpu6050
-    import Adafruit_PCA9685
-    import servo
+import time
+import mpu6050
+import Adafruit_PCA9685
+import servo
 
 
-    #direccion_driver = 0x60         #estos dos valores son orientativos, hay que cambiarlos
+#direccion_driver = 0x60         #estos dos valores son orientativos, hay que cambiarlos
 
 
-    #creamos el objeto driver
-    driver = Adafruit_PCA9685.PCA9685()
-    #creamos el objeto giroscopio
+#creamos el objeto driver
+driver = Adafruit_PCA9685.PCA9685()
 
 
-    print("se ha entrado en el modo debug")
-    #esperamos dos segundos
-    time.sleep(2)
 
-    NUMERO_SERVOS = 2       #este será el numero de servos que tendremos en nuestro robot
-    RANGO_MAXIMO = 20      #este será el angulo maximo al que se moveran los servos
-    RANGO_MINIMO = 0        #rango minimo al que se mueven los servos
+print("se ha entrado en el modo debug")
+#esperamos dos segundos
+time.sleep(2)
 
-    driver.set_pwm(0, 0, 0)
-    time.sleep(1)
-    driver.set_pwm(0, 0,90)
-    #movemos todos los servos, hasta su rango maximo
-    """for i in range(0,NUMERO_SERVOS):
-        for j in range(RANGO_MINIMO, RANGO_MAXIMO):
-            servo.mover_servo(i,j)
-            time.sleep(0.1)
-    """
-    print("se han terminado de mover los servos")
+NUMERO_SERVOS = 2       #este será el numero de servos que tendremos en nuestro robot
+RANGO_MAXIMO = 20      #este será el angulo maximo al que se moveran los servos
+RANGO_MINIMO = 0        #rango minimo al que se mueven los servos
+
+driver.set_pwm(0, 0, 0)
+time.sleep(1)
+driver.set_pwm(0, 0,90)
+#movemos todos los servos, hasta su rango maximo
+"""for i in range(0,NUMERO_SERVOS):
+    for j in range(RANGO_MINIMO, RANGO_MAXIMO):
+        servo.mover_servo(i,j)
+        time.sleep(0.1)
+"""
+print("se han terminado de mover los servos")
