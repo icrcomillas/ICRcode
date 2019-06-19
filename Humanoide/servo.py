51552b782a28b871df9d74b6c73a993a55936b6f
rulopelon <48516780+rulopelon@users.noreply.github.com>
2019-06-19 13:10:21 +0200

    .

--- Humanoide/servo.py
+++ Humanoide/servo.py
@@ -13,9 +13,9 @@ def calcular_pulso(ang):
 def mover_servo(n_servo,angulo):
     #posicion la reciviremos en un rango de 0 a 4096
 
-    if angulo >= robot.angulo_maximo or angulo <= robot.angulo_minimo:
+    if angulo > 180 or angulo < 0:
         print("no se puede mover ese rango, esta fuera del alcance")
-    elif angulo <= robot.angulo_maximo and angulo >= robot.angulo_minimo:
+    elif angulo <= 180 and angulo >= 0:
 
         pulso = calcular_pulso(angulo)
         driver.set_pwm(n_servo, 0, pulso)
