ba562ba95dba8f2ad7bc7609503ed6b71406c9f7
rulopelon <rulopelon@gmail.com>
2020-01-03 13:11:20 +0100

    inteligencia artificial

--- Humanoide/codigo/simulacion.py
+++ Humanoide/codigo/simulacion.py
@@ -11,41 +11,26 @@ from collections import deque
 import time
 import random
 import os
-
-DISCOUNT = 0.99
-REPLAY_MEMORY_SIZE = 50_000  # How many last steps to keep for model training
-MIN_REPLAY_MEMORY_SIZE = 1_000  # Minimum number of steps in a memory to start training
-MINIBATCH_SIZE = 64  # How many steps (samples) to use for training
-UPDATE_TARGET_EVERY = 5  # Terminal states (end of episodes)
-MODEL_NAME = 'prueba1'
-MIN_REWARD = -200  # For model save
-MEMORY_FRACTION = 0.20
+import numpy as np
+from dqn_keras import Agent
+from utils import plotLearning
 
 # Environment settings
 NUMERO_EPISODIOS = 20_000
-
-# Exploration settings
-epsilon = 1  # not a constant, going to be decayed
-EPSILON_DECAY = 0.99975
-MIN_EPSILON = 0.001
-
-#  Stats settings
-AGGREGATE_STATS_EVERY = 50  # episodes
-SHOW_PREVIEW = False
-
 RECOMPENSA_ITERACION = 1 #recompensa que se da por cumplir cada iteración
-MARGEN_CAIDA = 2 #altura a la que se considera que ha caido el robot
-ID_ROOT = 4 #id del link utilizado para coger la velocidad total del objeto
+MARGEN_CAIDA = 10 #altura a la que se considera que ha caido el robot
+ID_ROOT = 10#id del link utilizado para coger la velocidad total del objeto
 POSICION_INICIAL = [0,0,8]
 ORIENTACION_INICIAL=[45,0,0,45]
 PENALIZACION = 1
 
 FUERZA_MAXIMA =500              #FUERZA MÁXIMA QUE SE APLICA EN CADA UNION
 INCREMENTO_UNION = 0.5          #CUANTO SE SUMO RESTA CADA UNION EN CADA ITERACION
+NUMERO_SERVOS = 15
 
 #clase del entorno de simulacion
 class entorno():
-    DIMENSION_OBSERVACION = 49
+    DIMENSION_OBSERVACION = 51
     DIMENSION_ACCION= 3
     def __init__(self):
         self.entorno = p.connect(p.GUI)     
@@ -80,21 +65,32 @@ class entorno():
         p.stepSimulation()
         #hay que crear el reward
         #vemos el estado del entorno despues de eejcutar la accion
-        estado_actual,posicion_actual= self.estado()
+        estado_actual= self.estado()
+        estado_actual[50] = servo
+      
 
         score = self.reward(estado_actual)
-        flag  = False  #booleano utilizado para establecer si se ha llegado al final del proceso o no
+        if estado_actual[49] ==1:
+            flag = True
+        else:
+            flag = False  #booleano utilizado para establecer si se ha llegado al final del proceso o no
 
         return score,estado_actual,flag
     def accion(self,accion, servo):
+        estado = self.estado()
         indice = servo-1
-        if accion == [1,0,0]:
+        posicion = 0
+        #print(accion)
+        
+        if accion == 0:
            posicion = estado[indice] + INCREMENTO_UNION         #mira la posicion el momento y la aumenta
-        elif accion == [0,1,0]:
+        elif accion == 2:
             posicion = estado[indice]-INCREMENTO_UNION          #mira la posicion el momento y la diminuye
-        elif accion == [0,0,1]:
+        elif accion == 1:
             posicion = estado[indice]
-        p.setJointMotorControl2(self.robot, jointIndex = servo,controlMode = p.POSITION_CONTROL,targePosition = posicion,force =FUERZA_MAXIMA)
+        
+        
+        p.setJointMotorControl2(self.robot, jointIndex = servo,controlMode = p.POSITION_CONTROL,targetPosition = posicion,force =FUERZA_MAXIMA)
         p.stepSimulation()
         return
     def reward(self,estado):
@@ -124,231 +120,64 @@ class entorno():
         else: 
             caido = 0
         #ver si los dos pies estás paralelos (opcional)
-        
-        info_uniones = np.hstack((posicion,velocidad,torque,WorldOrientation,caido))
-        info_uniones = np.round(info_uniones,2)
-        return info_uniones, WorldOrientation
+        servo = 0
+        info_uniones = np.hstack((posicion,velocidad,torque,WorldOrientation,caido,servo))
+        info_uniones = np.around(info_uniones,1)
+    
+        return info_uniones
  
  
-entorno = entorno()
-
-# For stats
-ep_rewards = [-200]
-
-# For more repetitive results
-random.seed(1)
-np.random.seed(1)
-tf.set_random_seed(1)
-
-
-# Own Tensorboard class
-class ModifiedTensorBoard(TensorBoard):
-
-    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.step = 1
-        self.writer = tf.summary.FileWriter(self.log_dir)
-
-    # Overriding this method to stop creating default log writer
-    def set_model(self, model):
-        pass
-
-    # Overrided, saves logs with our step number
-    # (otherwise every .fit() will start writing from 0th step)
-    def on_epoch_end(self, epoch, logs=None):
-        self.update_stats(**logs)
-
-    # Overrided
-    # We train for one batch only, no need to save anything at epoch end
-    def on_batch_end(self, batch, logs=None):
-        pass
-
-    # Overrided, so won't close writer
-    def on_train_end(self, _):
-        pass
-
-    # Custom method for saving own metrics
-    # Creates writer, writes custom metrics and closes writer
-    def update_stats(self, **stats):
-        self._write_logs(stats, self.step)
-
-
-# Agent class
-class DQNAgent:
-    def __init__(self):
-
-        # Main model
-        self.model = self.create_model()
-
-        # Target network
-        self.target_model = self.create_model()
-        self.target_model.set_weights(self.model.get_weights())
-
-        # An array with last n steps for training
-        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
-
-        # Custom tensorboard object
-        self.tensorboard = ModifiedTensorBoard(log_dir="logs/{}-{}".format(MODEL_NAME, int(time.time())))
-
-        # Used to count when to update target network with main network's weights
-        self.target_update_counter = 0
-
-    def create_model(self):
-        model = Sequential()
-
-        model.add(Conv2D(256, (3, 3), input_shape=entorno.DIMENSION_OBSERVACION))  # OBSERVATION_SPACE_VALUES = (10, 10, 3) a 10x10 RGB image.
-        model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.2))
-
-        model.add(Conv2D(256, (3, 3)))
-        model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.2))
-
-        model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-        model.add(Dense(64))
-
-        model.add(Dense(entorno.DIMENSION_ACCION, activation='linear'))  # ACTION_SPACE_SIZE = how many choices (9)
-        model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['accuracy'])
-        return model
-
-    # Adds step's data to a memory replay array
-    # (observation space, action, reward, new observation space, done)
-    def update_replay_memory(self, transition):
-        self.replay_memory.append(transition)
-
-    # Trains main network every step during episode
-    def train(self, terminal_state, step):
-
-        # Start training only if certain number of samples is already saved
-        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
-            return
-
-        # Get a minibatch of random samples from memory replay table
-        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
-
-        # Get current states from minibatch, then query NN model for Q values
-        current_states = np.array([transition[0] for transition in minibatch])/255
-        current_qs_list = self.model.predict(current_states)
-
-        # Get future states from minibatch, then query NN model for Q values
-        # When using target network, query it, otherwise main network should be queried
-        new_current_states = np.array([transition[3] for transition in minibatch])/255
-        future_qs_list = self.target_model.predict(new_current_states)
-
-        X = []
-        y = []
-
-        # Now we need to enumerate our batches
-        for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
-
-            # If not a terminal state, get new q from future states, otherwise set it to 0
-            # almost like with Q Learning, but we use just part of equation here
-            if not done:
-                max_future_q = np.max(future_qs_list[index])
-                new_q = reward + DISCOUNT * max_future_q
-            else:
-                new_q = reward
-
-            # Update Q value for given state
-            current_qs = current_qs_list[index]
-            current_qs[action] = new_q
-
-            # And append to our training data
-            X.append(current_state)
-            y.append(current_qs)
-
-        # Fit on all samples as one batch, log only on terminal state
-        self.model.fit(np.array(X)/255, np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False, callbacks=[self.tensorboard] if terminal_state else None)
-
-        # Update target network counter every episode
-        if terminal_state:
-            self.target_update_counter += 1
-
-        # If counter reaches set value, update target network with weights of main network
-        if self.target_update_counter > UPDATE_TARGET_EVERY:
-            self.target_model.set_weights(self.model.get_weights())
-            self.target_update_counter = 0
-
-    # Queries main network for Q values given current observation space (environment state)
-    def get_qs(self, state):
-        return self.model.predict(np.array(state).reshape(-1, *state.shape)/255)[0]
-# For stats
-ep_rewards = [-200]
-
-# For more repetitive results
-random.seed(1)
-np.random.seed(1)
-tf.set_random_seed(1)
-
-# Memory fraction, used mostly when trai8ning multiple agents
-#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=MEMORY_FRACTION)
-#backend.set_session(tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)))
-
-# Create models folder
-if not os.path.isdir('models'):
-    os.makedirs('models')
 
 
 if __name__ == '__main__':
-    #creo el entorno
+    env = entorno()
 
-    agent = DQNAgent()
-    while True:
-        entorno.step()
-    # Iterate over episodes
-    for episode in range(NUMERO_EPISODIOS):
+    
+    fichero = "grafica_rendimiento.png"
+    num_games = 500
+    load_checkpoint = False
+    best_score = -21
+    agent = Agent(gamma=0.99, epsilon=1.0, alpha=0.0001,
+                  input_dims=(env.DIMENSION_OBSERVACION,), n_actions=env.DIMENSION_ACCION, mem_size=25000,
+                  eps_min=0.02, batch_size=32, replace=1000, eps_dec=1e-5)
+                  
 
-        # Update tensorboard step every episode
-        agent.tensorboard.step = episode
+    if load_checkpoint:
+        agent.load_models()
 
-        # Restarting episode - reset episode reward and step number
-        episode_reward = 0
-        step = 1
+    scores, eps_history = [], []
+    n_steps = 0
 
-        # Reset environment and get initial state
-        current_state = entorno.reset()
-
-        # Reset flag and start iterating until episode ends
+    for i in range(num_games):
         done = False
+        observation = env.reset()
+        score = 0
         while not done:
-
-            # This part stays mostly the same, the change is to query a model for Q values
-            if np.random.random() > epsilon:
-                # Get action from Q table
-                action = np.argmax(agent.get_qs(current_state))
-            else:
-                # Get random action
-                action = np.random.randint(0, entorno.ACTION_SPACE_SIZE)
-
-            new_state, reward, done = entorno.step(action)
-
-            # Transform new continous state to new discrete state and count reward
-            episode_reward += reward
-
-            # Every step we update replay memory and train main network
-            agent.update_replay_memory((current_state, action, reward, new_state, done))
-            agent.train(done, step)
-
-            current_state = new_state
-            step += 1
-
-        # Append episode reward to a list and log stats (every given number of episodes)
-        ep_rewards.append(episode_reward)
-        if not episode % AGGREGATE_STATS_EVERY or episode == 1:
-            average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
-            min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
-            max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
-            agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
-
-            # Save model, but only when min reward is greater or equal a set value
-            if min_reward >= MIN_REWARD:
-                agent.model.save(f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
-
-        # Decay epsilon
-        if epsilon > MIN_EPSILON:
-            epsilon *= EPSILON_DECAY
-            epsilon = max(MIN_EPSILON, epsilon)
+            for servo in range(NUMERO_SERVOS):      #vamos a hacer que se itere por cada servo de forma independiente
+                action = agent.choose_action(observation)
+                reward, observation_, done = env.step(action,servo)
+                n_steps += 1
+                score += reward
+                print("se ha llegado al punto 1")
+                if not load_checkpoint:
+                    agent.store_transition(observation, action,reward, observation_, int(done))
+                    agent.learn()
             
+                observation = observation_
+
+            scores.append(score)
+        
+        avg_score = np.mean(scores[-100:])
+        print('episode: ', i,'score: ', score,
+             ' average score %.3f' % avg_score,
+            'epsilon %.2f' % agent.epsilon, 'steps', n_steps)
+        if avg_score > best_score:
+            agent.save_models()
+            print('avg score %.2f better than best score %.2f, saving model' % (
+                  avg_score, best_score))
+            best_score = avg_score
+
+        eps_history.append(agent.epsilon)
+
+    x = [i+1 for i in range(num_games)]
+    plot_learning_curve(x, scores, eps_history, fichero)
