b46956af41b5716b28f7c247b2c8704b33ae25e8
rulopelon <48516780+rulopelon@users.noreply.github.com>
2021-03-16 14:08:23 +0100

    Fichero para realizar toda la configuracion de forma automatica

---
+++ configuracion.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt upgrade
+Y
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+Y
+sudo apt install python3.6
+Y
+sudo apt install python3-pip
+pip3 install numpy scipy matplotlib ipython pandas sumpy nose
+pip3 install libiio
+pip3 install pyadi-iio[jesd]
