8715d591b7b1d76e799a671b9d330e6466cf58b3
rulopelon <48516780+rulopelon@users.noreply.github.com>
2021-03-19 19:26:46 +0100

    claridad codigo

--- Torreta/plutoControler.py
+++ Torreta/plutoControler.py
@@ -10,12 +10,12 @@ import adi
 with open('Torreta\configuracion.json') as json_file:
     ficheroJson = json.load(json_file)
     
-placaPluto = adi.Pluto(ficheroJson['direccionIp'])
 
 
 if ficheroJson['test']:
     #en el caso de que nos encontremos en modo de test
-    
+    grafica = graficas()
+
     samplerate, data = wavfile.read('Torreta\prueba.wav')
     #se crea el vector de tiempos
     vector_tiempo = np.linspace(0,len(data)/samplerate,len(data))
@@ -24,16 +24,19 @@ if ficheroJson['test']:
     #se realiza el calculo del espectro de la señal
     fft_data = fft(data)
     vector_frecuencia = fftfreq(len(data),1/samplerate)
-    grafica = graficas()
+    
     grafica.mostrarGrafica(vector_frecuencia,np.abs(fft_data),'Espectro')
     #se realiza el diezmado de la señal
+else:
+    placaPluto = adi.Pluto(ficheroJson['direccionIp'])
+    inicializarPlaca()
 
 
 def inicializarPlaca():
     #se ponen los valores por defecto a la placa, para poder recibir una señal 
     setPortadoraRecepcion(ficheroJson['c_rx_default'])
     setPortadoraTransmision(ficheroJson['c_tx_default'])
-    
+
 def setPortadoraRecepcion(frecuencia):
     placaPluto.rx_lo(frecuencia)
 def setPortadoraTransmision(frecuencia):
