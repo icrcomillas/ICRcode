612e039d40d4c5c5ae66c529f0c5016ca676c935
rulopelon <48516780+rulopelon@users.noreply.github.com>
2021-03-21 21:09:36 +0100

    metodos staticos

--- Torreta/operaciones.py
+++ Torreta/operaciones.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from scipy.fft import fft, fftfreq
 
 class graficas():
     def __init__(self):
         super().__init__()
+        
+    @staticmethod
     def mostrarGrafica(self,datosx,datosy,titulo):
         fig, ax = plt.subplots()
         #se realiza el calculo del espectro de la señal
@@ -13,7 +16,17 @@ class graficas():
 class operacion():
     def __init__(self):
         super().__init__()
+    @staticmethod
     def calcularEspectro(self, data,samplerate):
         fft_data = fft(data)
         vector_frecuencia = fftfreq(len(data),1/samplerate)
         return fft_data, vector_frecuencia
+class sistema():
+    def __init__(self):
+        super().__init__()
+    @staticmethod
+    def guardarArray(self,path,datos):
+        np.save(path,datos)
+    @staticmethod
+    def cargaraArray(self,path):
+        return np.load(path)
