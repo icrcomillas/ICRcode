09e3c957c490a99abecab1cfcd041f1e42076d91
rulopelon <48516780+rulopelon@users.noreply.github.com>
2021-03-31 18:35:22 +0200

    Update

--- Torreta/configuracion.sh
+++ Torreta/configuracion.sh
@@ -18,7 +18,26 @@ cd ..
 sudo apt install python3-pip
 pip install --upgrade "pip < 21.0"
 pip3 install numpy scipy matplotlib ipython pandas sumpy nose
-pip3 install libiio
+sudo apt-get install git libxml2 libxml2-dev bison flex libcdk5-dev cmake python3-pip
+
+git clone https://github.com/analogdevicesinc/libiio.git
+cd libiio
+cmake ./
+make all -j4
+sudo make install
+sudo ldconfig
+cd bindings/python/
+sudo python3 setup.py.cmakein install
+cd ..
+
+
+git clone https://github.com/analogdevicesinc/libad9361-iio.git
+cd libad9361-iio
+cmake ./
+make -j3
+sudo make install
+cd ..
+
 git clone https://github.com/analogdevicesinc/pyadi-iio.git
 cd pyadi-iio
 sudo python3 setup.py install
