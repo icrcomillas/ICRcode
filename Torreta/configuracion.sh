8ab141b3b9a169432e8ab2a5c826b523853f2ff7
rulopelon <48516780+rulopelon@users.noreply.github.com>
2021-03-31 19:03:46 +0200

    simplificacion

--- Torreta/configuracion.sh
+++ Torreta/configuracion.sh
@@ -7,14 +7,7 @@ sudo apt update
 Y
 sudo apt install git
 Y
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
-wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz
-tar -xf Python-3.8.0.tgz
-cd Python-3.8.0
-./configure --enable-optimizations
-make -j 8
-sudo make altinstall
-cd ..
+sudo apt instasll python3.7
 sudo apt install python3-pip
 pip install --upgrade "pip < 21.0"
 pip3 install numpy scipy matplotlib ipython pandas sumpy nose
