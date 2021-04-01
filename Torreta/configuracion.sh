ee11add6201d6cb68d595d78214e2b0b0ef8953b
rulopelon <48516780+rulopelon@users.noreply.github.com>
2021-04-01 13:54:56 +0200

    Adaptacion python3.7

--- Torreta/configuracion.sh
+++ Torreta/configuracion.sh
@@ -2,15 +2,16 @@ sudo apt update
 sudo apt upgrade
 Y
 sudo apt-get purge python3:i386
+sudo apt-get purge python3
 sudo apt autoremove
 sudo apt update
 Y
 sudo apt install git
 Y
 sudo apt instasll python3.7
-sudo apt install python3-pip
+python3.7 -m pip install pip
 pip install --upgrade "pip < 21.0"
-pip3 install numpy scipy matplotlib ipython pandas sumpy nose
+python3.7 -m pip install numpy scipy matplotlib ipython pandas sumpy nose
 sudo apt-get install git libxml2 libxml2-dev bison flex libcdk5-dev cmake python3-pip
 
 git clone https://github.com/analogdevicesinc/libiio.git
@@ -20,7 +21,7 @@ make all -j4
 sudo make install
 sudo ldconfig
 cd bindings/python/
-sudo python3 setup.py.cmakein install
+sudo python3.7 setup.py.cmakein install
 cd ..
 
 
@@ -33,4 +34,4 @@ cd ..
 
 git clone https://github.com/analogdevicesinc/pyadi-iio.git
 cd pyadi-iio
-sudo python3 setup.py install
+sudo python3.7 setup.py install
