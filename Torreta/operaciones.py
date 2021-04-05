73e407929791daa9c57559063483e7189e56c169
rulopelon <48516780+rulopelon@users.noreply.github.com>
2021-04-05 17:47:54 +0200

    generacion ruido y analisis espectral

--- Torreta/operaciones.py
+++ Torreta/operaciones.py
@@ -1,16 +1,27 @@
 
 import numpy as np
-import json
-
-
-
 
 class Sistema():
     def __init__(self):
         super().__init__()
-    @staticmethod
-    def guardarArray(self,path,datos):
-        np.savetxt(path,datos)
-    @staticmethod
-    def cargaraArray(self,path):
-        return np.loadtxt(path)
+
+    def analizarEspectroFrecuencia(self,arrayDatos,arrayFrecuencias,threshold):
+        frecuencia = -1
+        for i in range(0,len(arrayDatos)):
+            if arrayDatos[i] > threshold:
+                #en el caso de que un valor sea superior al esperado, busca la frecuencia
+                frecuencia = arrayFrecuencias[i]
+        return frecuencia
+
+    def analizarEspectro(self,arrayDatos,arrayFrecuencias ,f_target, f_threshold, f_carrier, samplerate):
+
+        f = f_target - f_carrier # frecuencia buscada 
+        delta_f = samplerate / (len(arrayDatos) - 1) # intervalo entre frec. 
+        pos = (f - samplerate) / delta_f # posicion del valor de la frecuencia buscado
+
+        if arrayFrecuencias(pos) >= f_threshold:
+            return True
+        else:
+            return False
+    def generarRuido(self,media,varianza,longitud):
+        return np.random.normal(media,varianza,longitud)
