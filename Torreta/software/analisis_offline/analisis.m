e7f10af761c1656ee0ac562fe0e52055a00943a3
rulopelon <48516780+rulopelon@users.noreply.github.com>
2021-04-07 10:11:11 +0200

    reorganizacion

---
+++ Torreta/software/analisis_offline/analisis.m
@@ -0,0 +1,34 @@
+close all
+clear
+fs = 40000;
+threshold = 8;
+%se leen los archivos y se calcula el error de la fft obtenida y la
+%calculada
+ficheroFft = "datosNormales.txt";
+fileIdFft = fopen(ficheroFft,'r');
+datosFft = fscanf(fileIdFft,'%f',100000);
+fclose(fileIdFft);
+fft = fft(datosFft,length(datosFft))/length(datosFft);
+fft = fftshift(abs(fft));
+figure
+plot(fft)
+
+% for i = 0:5
+%     
+%     ficheroFft = "fft"+i+".txt";
+%     fileIdFft = fopen(ficheroFft,'r');
+%     datosFft = fscanf(fileIdFft,'%f',40000);
+%     fclose(fileIdFft);
+%     figure
+%     hold on
+%     for k = 1:length(datosFft)
+%         if abs(datosFft(k))< threshold
+%             datosFft(k) = 0;
+%         end
+%     end
+%     
+%     stem(datosFft)
+%     %plot(20*log10(datosFft),'*')
+%     
+% end
+% 
