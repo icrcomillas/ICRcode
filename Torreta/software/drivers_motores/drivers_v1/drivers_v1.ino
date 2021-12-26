e645e5eb7543a9e9feb633b1696fbb6b5a305979
rulopelon <48516780+rulopelon@users.noreply.github.com>
2021-12-26 12:03:18 +0100

    Driver motores terminado

--- Torreta/software/drivers_motores/drivers_v1/drivers_v1.ino
+++ Torreta/software/drivers_motores/drivers_v1/drivers_v1.ino
@@ -8,14 +8,15 @@
 #define MOTOR_STEPS 200
 #define RPM 120
 
-#define DIR_YAW 8
-#define STEP_YAW 9
-#define DIR_TILT 8
-#define STEP_TILT 9
+#define DIR_YAW 5
+#define STEP_YAW 2  //x
+#define DIR_TILT 6  
+#define STEP_TILT 3 //y
 #define SLEEP 13 // optional (just delete SLEEP from everywhere if not used)
 
 DRV8825 Tilt(MOTOR_STEPS, DIR_TILT, STEP_TILT, SLEEP, MODE0, MODE1, MODE2);
 DRV8825 Yaw(MOTOR_STEPS, DIR_YAW, STEP_YAW, SLEEP, MODE0, MODE1, MODE2);
+
 void setup() {
     /*
      * Microstepping mode: 1, 2, 4, 8, 16 or 32 (where supported by driver)
@@ -24,6 +25,12 @@ void setup() {
      * The motor should rotate just as fast (at the set RPM),
      * but movement precision is increased, which may become visually apparent at lower RPMs.
      */
+    pinMode(DIR_YAW,OUTPUT);
+    pinMode(STEP_YAW,OUTPUT);
+    pinMode(DIR_TILT,OUTPUT);
+    pinMode(STEP_TILT,OUTPUT);
+
+    
     Yaw.begin(RPM);
     // if using enable/disable on ENABLE pin (active LOW) instead of SLEEP uncomment next line
     // stepper.setEnableActiveState(LOW);
@@ -36,9 +43,10 @@ void setup() {
   
   //Se establece la velocidad del puerto serial
   Serial.begin(115200);
-  while(!Serial.available()){
-    
-  }
+//  while(!Serial.available()){
+//    
+//  }
+moverYaw(360,1);
   
 }
 
@@ -46,6 +54,7 @@ void loop() {
  //se ejecuta el código mientras el puerto serial se mantenga abierto
  while(Serial.available()>0){
       String data = Serial.readStringUntil(")");
+      
       /*El formato de mensajes que se van a recibir es:
        * YAW,angulo(3 posiciones),Dir,TILT,angulo(3 posiciones),Dir)
        * YAW y TILT no se envían, es unicamente para saber el orden de recepción
@@ -70,11 +79,9 @@ void loop() {
       // Se convierte a Int
       int  yaw = stringYaw.toInt();
       int tilt = stringTilt.toInt();
-      
       // Se mueven los motores
       moverYaw(yaw,dirYaw);
       moverTilt(tilt,dirTilt);
-    
  }
 
 }
@@ -85,7 +92,6 @@ void moverYaw(int angulo,int direccion){
   }else{
     angulo = angulo;
   }
-  
    Yaw.rotate(angulo);     // forward revolution + reverse -
 }
 void moverTilt(int angulo, int direccion){
@@ -95,6 +101,6 @@ void moverTilt(int angulo, int direccion){
   }else{
     angulo = angulo;
   }
-  
+   
    Tilt.rotate(angulo);     // forward revolution + reverse -
 }
